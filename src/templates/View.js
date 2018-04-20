32dcb5ebd5f7792b788e69e78a22e9b69c1f6c2f
vincenx <freakypinky777@gmail.com>
2018-04-20 21:25:33 +0600

    Add moment

--- src/templates/View.js
+++ src/templates/View.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import renderHTML from 'react-render-html'
 
 const View = (props) => (
-	<div>
-		<h1>
-			{ props.title }
-		</h1>
-		<p>
-			{ props.body }
-		</p>
+	<div className="view">
+		<div className="view__heading">
+			<small class="text-muted">10/05/2018</small>
+			<h2 className="view__title">
+				{ props.title }
+			</h2>
+		</div>
+		<div className="view__body">
+			{renderHTML(props.body)}
+		</div>
 	</div>
 )
 export default connect(
